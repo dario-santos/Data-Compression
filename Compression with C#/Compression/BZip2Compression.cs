5967761cba5f51bc7222ebab50a13dfbe52fe7aa
Dário Santos <dariovfsantos@gmail.com>
2019-05-16 13:30:57 +0100

    Added C# compression and decompression
    Added GZip and BZip2 algorithms.

---
+++ Compression with C#/Compression/BZip2Compression.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Diagnostics;
+using ICSharpCode.SharpZipLib.BZip2;
+
+namespace Compression
+{
+    /// <summary>
+    /// BZip2 compression algorithm.
+    /// </summary>
+    class BZip2Compression : CompressionAlgorithm
+    {
+        public BZip2Compression(string extension, int minCompressionLevel = 1, int maxCompressionLevel = 9)
+            : base(extension, minCompressionLevel, maxCompressionLevel)
+        {
+        }
+
+        /// <summary>
+        /// Compresses a given file using the BZip2 Algorithm.
+        /// </summary>
+        /// <param name="fInput">The file to compress.</param>
+        /// <param name="dataUnit">The unit that will be saved in the log file</param>
+        public override void Compress(FileInfo fInput = null, DataUnits dataUnit = DataUnits.Byte)
+        {
+            StreamWriter fLog = File.AppendText("log_compress.txt");
+            Stopwatch timer = new Stopwatch();
+            FileInfo info = null;
+
+            for(int i = MinCompressionLevel ; i <= MaxCompressionLevel ; i++)
+            {
+                timer.Start();
+
+                using(FileStream fInputStream = fInput.OpenRead())
+                    using(FileStream fOutputStream = File.Create(fInput.FullName + i.ToString() + Extension))
+                        BZip2.Compress(fInputStream, fOutputStream, true, level:i);
+
+                timer.Stop();
+
+                info = new FileInfo(fInput.FullName + i + Extension);
+
+                fLog.WriteLine(i + " " + Byte.ConvertTo(info.Length, dataUnit) + " " + timer.Elapsed.TotalSeconds.ToString("n2") + " BZIP2");
+            }
+            fLog.Close();
+        }
+
+        /// <summary>
+        /// Decompresses the given files using the BZip2 algorithm.
+        /// </summary>
+        /// <param name="fCompressed">The name of the file to decompress.</param>
+        public override void Decompress(string fName = null)
+        {
+            FileInfo fInput = new FileInfo(fName + MaxCompressionLevel + Extension);
+            StreamWriter fLog = File.AppendText("log_decompress.txt");
+            Stopwatch timer = new Stopwatch();
+
+            timer.Start();
+
+            using(FileStream fInputStream = fInput.OpenRead())
+                using(FileStream fOutputStream = File.Create(fName))
+                    BZip2.Decompress(fInputStream, fOutputStream, true);
+
+            timer.Stop();
+
+            fLog.WriteLine(fName + timer.Elapsed.TotalSeconds.ToString("n2") + " BZIP2");
+
+            fLog.Close();
+        }
+    }
+}
---
+++ Compression with C#/Compression/Byte.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Compression
+{
+    //TODO: Think about a better name to resolve the ambiguity between System.Byte and Compression.Byte
+    /// <summary>
+    /// The Byte methods
+    /// </summary>
+    static class Byte
+    {
+        /// <summary>
+        /// Converts a value in Byte to the given data unit.
+        /// </summary>
+        /// <param name="value">The value to convert</param>
+        /// <param name="dataUnit">The aimed unit.</param>
+        /// <returns>The converted value or -1 in the case of an error.</returns>
+        public static long ConvertTo(long value, DataUnits dataUnit)
+        {
+            switch(dataUnit)
+            {
+                case DataUnits.Byte:
+                    return value;
+
+                case DataUnits.KiloByte:
+                    return value / 1024;
+
+                case DataUnits.MegaByte:
+                    return value / (1024 * 1024);
+
+                case DataUnits.GigaByte:
+                    return value / (1024 * 1024 * 1024);
+
+                default:
+                    Console.WriteLine("\n\nError in ConvertByteTo, invalid DataType\n\n");
+                    return -1;
+            }
+        }
+    }
+}
