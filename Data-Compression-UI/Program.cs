c66108fd1fc8b0bdbff5de685d64af41789374af
Dario <dariovfsantos@gmail.com>
2019-05-17 18:48:25 +0100

    Some small stuff

--- Data-Compression-UI/Program.cs
+++ Data-Compression-UI/Program.cs
@@ -11,7 +11,7 @@ namespace Data_Compression_UI
         
         //Programs paths
         private static string CsCompressionPath=@"CSharpCompression\Program.exe";
-        private static string PyCompressionPath=@"source.py";
+        private static string PyCompressionPath=@"CompressionPython/source.py";
 
         //Files to compress paths
         private static List<string> filePaths = new List<string>();
@@ -19,38 +19,35 @@ namespace Data_Compression_UI
         static void Main(string[] args)
         {
             Inicializar();
-
+            
             StreamWriter temp = null;
             foreach(string path in filePaths)
             {
                 
-                temp = File.CreateText("gzip" + path.Split('\\')[1] + ".txt");
+                temp = File.CreateText("gzip" + path.Split('/')[1] + ".txt");
                 temp.Close();
-                temp = File.CreateText("bzip2" + path.Split('\\')[1] + ".txt");
+                temp = File.CreateText("bzip2" + path.Split('/')[1] + ".txt");
                 temp.Close();
 
                 //ExecuteProgramm(CsCompressionPath, path);
-                ExecuteProgramm(PyCompressionPath);
+                ExecuteProgramm("python3", PyCompressionPath + " " + path);
             }
         }
 
         private static void Inicializar()
         {
-            filePaths.Add(@"Recusros\pequena.svg");
-            filePaths.Add(@"Recusros\media.svg");
-            filePaths.Add(@"Recusros\grande.svg");
+            filePaths.Add(@"Recursos/media.svg");
+            filePaths.Add(@"Recusros/media.svg");
+            filePaths.Add(@"Recusros/grande.svg");
         }
 
-        private static void ExecuteProgramm(string path )//,string arg)
+        private static void ExecuteProgramm(string path ,string arg)
         {
             ProcessStartInfo startInfo = new ProcessStartInfo();
 	        startInfo.FileName = path;
-//	        startInfo.Arguments = arg;
-	        int id = Process.Start(startInfo).Id;
-    
-            if(Process.GetProcessById(id) != null)
-                Process.GetProcessById(id).WaitForExit();
-
+	        startInfo.Arguments = arg;
+	        Process p = Process.Start(startInfo);
+            p.WaitForExit();
         }
     }
 }
